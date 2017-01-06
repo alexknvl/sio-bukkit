b77a2647feac8e11f4bfc709e1d6c84bc097f3c0
Alexander Konovalov <alex.knvl@gmail.com>
2017-01-07 01:51:56 +0300

    Fixed the example project to do something useful.

--- example/src/main/scala/sio/bukkit/example/Main.scala
+++ example/src/main/scala/sio/bukkit/example/Main.scala
@@ -1,36 +1,13 @@
 package sio.bukkit.example
 
-import sio.bukkit.core.Plugin
-import sio.core.{SafeApp, IO}
+import sio.bukkit.core.{Server, Plugin}
+import sio.core.IO
 import sio.teletype._
 
-import scala.collection.mutable.ArrayBuffer
-
 class Main extends Plugin.Impl[Unit](new Plugin.Safe[Unit] {
-  override def enable: IO[Unit] = {
-    System.err.println("1")
-    var r: IO[Unit] = null
-    try {
-      r = IO.trace("test")
-    } catch {
-      case e: Throwable =>
-        System.err.println("1.1")
-        val buffer: ArrayBuffer[StackTraceElement] = ArrayBuffer.empty
-        var last: StackTraceElement = null
-        e.getStackTrace.foreach { s =>
-          if (s != last) {
-            buffer.append(s)
-            last = s
-          }
-        }
-        buffer.foreach(s => System.err.println(s"${s.getClassName} ${s.getMethodName}"))
-        System.err.println(e.getClass.getSimpleName)
-    }
-    System.err.println("2")
-    r
-  }
+  override def enable: IO[Unit] = for {
+    version <- Server.getServerVersion
+    address <- Server.getServerAddress
+    _       <- putStrLn(s"Hello! Running $version on $address")
+  } yield ()
 })
-
-object Main extends SafeApp {
-  def run(args: List[String]): IO[Unit] = putStrLn("Hello.")
-}
