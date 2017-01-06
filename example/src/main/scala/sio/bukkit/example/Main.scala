053c7dfa1ba61b31cc6eb3ccc37aff996d9114b0
Alexander Konovalov <alex.knvl@gmail.com>
2017-01-07 01:54:01 +0300

    A slightly better encoding for the Main class/object.

--- example/src/main/scala/sio/bukkit/example/Main.scala
+++ example/src/main/scala/sio/bukkit/example/Main.scala
@@ -4,10 +4,12 @@ import sio.bukkit.core.{Server, Plugin}
 import sio.core.IO
 import sio.teletype._
 
-class Main extends Plugin.Impl[Unit](new Plugin.Safe[Unit] {
+object Main extends Plugin.Safe[Unit] {
   override def enable: IO[Unit] = for {
     version <- Server.getServerVersion
     address <- Server.getServerAddress
     _       <- putStrLn(s"Hello! Running $version on $address")
   } yield ()
-})
+}
+
+class MainImpl extends Plugin.Impl[Unit](Main)
